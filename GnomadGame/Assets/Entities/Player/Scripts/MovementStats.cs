5bed65fae507152a606c2c9264679ba79f0b739f
MacroMew <33503006+Kyle-Mar@users.noreply.github.com>
2023-08-30 13:17:11 -0400

    Player Movement Jumping
    There is no horizontal movement.
    
    Added: MovementStats.cs
    Added PlayerMovement.cs

---
+++ GnomadGame/Assets/Entities/Player/Scripts/MovementStats.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/MovementStats", order = 1)]
+
+public class MovementStats
+{
+    public static float moveSpeed = 1;
+    public static float moveInertia = 1;
+    //intended to be able to change move speed in air.
+    public static float jumpMoveSpeed = 1f;
+    //Controls the time for how long the player can jump.
+    public static float maxJumpHeight = 0.1f;
+    //Force Multiplier for jumping.
+    public static float jumpSpeed=15;
+}
---
+++ GnomadGame/Assets/Entities/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] LayerMask groundLayerMask;
+    [SerializeField] float startJumpTime;
+    [SerializeField] float maxJumpTime;
+    
+    [SerializeField] BoxCollider2D col;
+    [SerializeField] Rigidbody2D rb;
+    
+    [SerializeField] bool isJumping = false;
+    [SerializeField] bool isGrounded = false;
+    
+    private void Start()
+    {
+        groundLayerMask = LayerMask.GetMask("Ground");
+        col = gameObject.GetComponent<BoxCollider2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        IsGrounded();
+        // We must ensure that we are grounded and not jumping before we begin to jump.
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping && isGrounded)
+        {
+            isJumping = true;
+            // set up future time that we will stop jumping unless the player stops first.
+            startJumpTime = Time.time;
+            maxJumpTime = startJumpTime + MovementStats.maxJumpHeight;
+            rb.velocity = new Vector2(rb.velocity.x, MovementStats.jumpSpeed);
+        }
+        // while the player is holding the spacebar.
+        // continue jumping as long as the timer will allow us.
+        else if (Input.GetKey(KeyCode.Space) && isJumping && (maxJumpTime > Time.time))
+        {
+            rb.AddForce(Vector2.up * MovementStats.jumpSpeed);
+        }
+        else 
+        {
+            // TODO: Implement non-frame independent lerp later
+            rb.velocity = Vector2.Lerp(rb.velocity, new Vector2(rb.velocity.x, Physics.gravity.y*5), Time.deltaTime);
+            isJumping = false;
+        }
+    }
+
+    void IsGrounded()
+    {
+        // Check if the player is grounded with a boxcast instead of a raycast.
+        // If necessary it can be changed to 2 raycasts on the bottom corners. 
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, col.bounds.extents, 0f, -transform.up, 1f, groundLayerMask);
+        if(hit.collider && col.IsTouchingLayers(groundLayerMask))
+        {
+            Debug.Log("test");
+            isGrounded = true;
+        }else 
+        {
+            isGrounded = false;
+        }
+    }
+}
