f9df86b816455c8492e691e7883ca5887078c4f3
MacroMew <33503006+Kyle-Mar@users.noreply.github.com>
2023-09-25 10:16:45 -0400

    Fix Merge Conflicts

--- GnomadGame/Assets/Entities/Player/Scripts/PlayerStateMachine.cs
+++ GnomadGame/Assets/Entities/Player/Scripts/PlayerStateMachine.cs
@@ -179,6 +179,7 @@ public class PlayerStateMachine : StateMachine
     private void UpdateMovementDirection(InputAction.CallbackContext cxt)
     {
        Vector2 inputVector = cxt.ReadValue<Vector2>();
+        Debug.Log(inputVector);
 
         if (inputVector == Vector2.left)
         {
