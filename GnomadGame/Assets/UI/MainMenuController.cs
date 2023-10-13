7acfcdcfafb7ffd976661643e36bcd1f3415dadf
WickMod <elijah_wickerham@yahoo.com>
2023-10-13 17:40:56 -0400

    TMP main menu
    To use level select, move mouse across slider. Be sure collapse is disabled in the debug log so you can easily see what you set the number to

--- GnomadGame/Assets/UI/MainMenuController.cs
+++ GnomadGame/Assets/UI/MainMenuController.cs
@@ -4,27 +4,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using Unity.VisualScripting;
 
 public class MainMenuController : MonoBehaviour
 {
-public void onClickPlay()
+    private int currentLevel;
+    [SerializeField] private GameObject mainMenuButtons;
+    [SerializeField] private GameObject levelSelectButtons;
+    //private GameObject currentMenu = mainMenuButtons;
+
+    private void Awake()
     {
+        currentLevel = 2;
+        Debug.Log("Press play to load Scene " + currentLevel);
+    }
+    public void OnClickPlay()
+    {
+        Debug.Log("Loading Master Scene and Scene " + currentLevel);
         SceneManager.LoadScene(1, LoadSceneMode.Additive);
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(currentLevel);
         //SceneManager.UnloadScene(0);
     }
+    public void OnClickLevelSelect()
+    {
+        mainMenuButtons.SetActive(false);
+        levelSelectButtons.SetActive(true);
+    }
 
-
-    public void onClickOptions()
+    public void OnClickOptions()
     {
         Debug.Log("Options Menu Not Implemented");
     }
 
-public void onClickExit()
+    public void OnClickExit()
     {
+        Debug.Log("Exit does not work from the editor");
         Application.Quit();
     }
 
+    public void OnClickBack()
+    {
+        mainMenuButtons.SetActive(true);
+        levelSelectButtons.SetActive(false);
+    }
 
+    public void OnChangecurrentLevel(System.Single newLevel)
+    {
+        currentLevel = (int)newLevel;        
+        Debug.Log("Press play to load Scene " + currentLevel);
+
+    }
 }
