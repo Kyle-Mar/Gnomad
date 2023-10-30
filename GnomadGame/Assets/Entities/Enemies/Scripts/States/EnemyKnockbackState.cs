c11c614b2b725cab8fd86b02db15dbb95c9206be
AndrewCramer3 <97630245+AndrewCramer3@users.noreply.github.com>
2023-10-30 11:25:42 -0400

    Volley Set-Up When Sliding
    When the player slides into an enemy, the enemy now moves up and slightly behind the player as we talked about. There are a few tiny bugs where the enemy sometimes doesn't have the right velocity when they are off the ground, but hopefully that will be fixed when volley is in. Also, I'm using the names of objects to decide the velocity of knockback and whether the enemy was slided into, so it will probably be needed to change.

--- GnomadGame/Assets/Entities/Enemies/Scripts/States/EnemyKnockbackState.cs
+++ GnomadGame/Assets/Entities/Enemies/Scripts/States/EnemyKnockbackState.cs
@@ -33,13 +33,16 @@ public class EnemyKnockbackState : EnemyBaseState
         InitializeSubState();
         knockbackTimer = context.KnockbackTimer;
         initialKnockbackDirection = context.LastKBDirection.normalized;
-        if (initialKnockbackDirection.x < 0.5f && initialKnockbackDirection.x > -0.5f)
+        if (!context.IsSlidedInto)
         {
-            initialKnockbackDirection.x = 0.5f * Mathf.Sign(initialKnockbackDirection.x);
-        }
-        if (initialKnockbackDirection.y < 0.5f && initialKnockbackDirection.y > -0.5f)
-        {
-            initialKnockbackDirection.y = 0.5f * Mathf.Sign(initialKnockbackDirection.y);
+            if (initialKnockbackDirection.x < 0.5f && initialKnockbackDirection.x > -0.5f)
+            {
+                initialKnockbackDirection.x = 0.5f * Mathf.Sign(initialKnockbackDirection.x);
+            }
+            if (initialKnockbackDirection.y < 0.5f && initialKnockbackDirection.y > -0.5f)
+            {
+                initialKnockbackDirection.y = 0.5f * Mathf.Sign(initialKnockbackDirection.y);
+            }
         }
         Debug.Log("Initial Knockback Direction + " + initialKnockbackDirection);
         context.rb.velocity = initialKnockbackDirection * 27.5f;
@@ -54,11 +57,19 @@ public class EnemyKnockbackState : EnemyBaseState
         knockbackTimer = 0f;
         SetSubState(context.MoveState);
         context.IsDamaged = false;
+        
     }
 
     public override void FixedUpdateState()
     {
-        context.rb.velocity = currentKnockbackVelocity + new Vector3(0,  (MovementStats.fallSpeed * Time.fixedDeltaTime));
+        if (!context.IsSlidedInto)
+        {
+            context.rb.velocity = currentKnockbackVelocity + new Vector3(0,  (MovementStats.fallSpeed * Time.fixedDeltaTime));
+        }
+        else
+        {
+            context.rb.velocity = currentKnockbackVelocity + new Vector3(0,  (MovementStats.fallSpeed * Time.fixedDeltaTime * 0.5f));
+        }
         currentKnockbackVelocity = context.rb.velocity;
     }
 
