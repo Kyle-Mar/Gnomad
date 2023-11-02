521016daf515e0dce89aa296180238edde3b91d6
MacroMew <33503006+Kyle-Mar@users.noreply.github.com>
2023-11-02 13:40:11 -0400

    Update TerrainBlockController.cs So No Build bad

--- GnomadGame/Assets/Zones/GenericAssetTools/Scripts/TerrainBlockController.cs
+++ GnomadGame/Assets/Zones/GenericAssetTools/Scripts/TerrainBlockController.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -129,4 +130,4 @@ public class TerrainBlockController : MonoBehaviour
     
 
 }
-
+#endif
